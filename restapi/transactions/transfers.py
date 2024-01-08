3ee90c47d38ab714696558210be1ae890391bc59
arturhgca <dev@mail.arturcoutinho.com>
2024-01-08 20:21:34 -0300

    Implement transfers controller

--- restapi/transactions/transfers.py
+++ restapi/transactions/transfers.py
@@ -1,31 +1,62 @@
+from datetime import datetime
 from decimal import Decimal
 from typing import List
 
+from budget.facts import Allocation, Transfer
+
 
 def create_transfer(
-    source: str, destination: str, amount: Decimal, date: str, notes: str = None
+        source: str,
+        destination: str,
+        amount: Decimal,
+        currency: str,
+        date: str,
+        notes: str = None,
 ) -> dict:
-    raise NotImplementedError
+    return Transfer.create(
+        source=source,
+        destination=destination,
+        amount=amount,
+        currency=currency,
+        date=datetime.strptime(date, "%Y-%m-%d"),
+        notes=notes,
+    ).as_dict()
 
 
 def get_transfers() -> List[dict]:
-    raise NotImplementedError
+    return [
+        item.as_dict()
+        for item in Transfer.get_all()
+    ]
 
 
 def get_transfer(uid: str) -> dict:
-    raise NotImplementedError
+    return Transfer.get(uid=uid).as_dict()
 
 
 def update_transfer(
-    uid: str,
-    source: str,
-    destination: str,
-    amount: Decimal,
-    date: str,
-    notes: str = None,
+        uid: str,
+        source: str,
+        destination: str,
+        amount: Decimal,
+        currency: str,
+        date: str,
+        notes: str = None,
 ) -> dict:
-    raise NotImplementedError
+    transfer = Transfer.get(uid=uid)
+    transfer.update(
+        source=source,
+        destination=destination,
+        amount=amount,
+        currency=currency,
+        date=datetime.strptime(date, "%Y-%m-%d"),
+        notes=notes,
+    )
+    return transfer.as_dict()
 
 
 def delete_transfer(uid: str) -> dict:
-    raise NotImplementedError
+    transfer = Transfer.get(uid=uid)
+    contents = transfer.as_dict()
+    transfer.delete()
+    return contents
